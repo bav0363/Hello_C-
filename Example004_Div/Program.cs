b727fb301888dcb390e5272198569dae015d4e63
Andrey_Bryzgalov <bav03630363@gmail.com>
2023-04-07 15:24:41 +0300

    Example 004 Div project/ path2

--- Example004_Div/Program.cs
+++ Example004_Div/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите первое число: ");
+double number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+double number2 = Convert.ToInt32(Console.ReadLine());
+double div = number1 / number2;
+Console.WriteLine("Результат деления первого числа на второе будет равен: " + div);
