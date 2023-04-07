f38c570afca0ce480ea6979c7276a737794ca946
Andrey_Bryzgalov <bav03630363@gmail.com>
2023-04-07 15:43:07 +0300

    Example 003 Div project/ path4

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,6 +1,15 @@
-﻿Console.Write("Введите первое число: ");
-int number1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите второе число: ");
-int number2 = Convert.ToInt32(Console.ReadLine());
+﻿// Console.Write("Введите первое число: ");
+// int number1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите второе число: ");
+// int number2 = Convert.ToInt32(Console.ReadLine());
+// int sum = number1 + number2;
+// Console.WriteLine("Сумма введенных чисел будет равна: " + sum);
+
+// Второй пример с использованием Рандома
+
+int number1 = new Random().Next(1, 100);
+Console.WriteLine("Первое рандомное число равно: " + number1);
+int number2 = new Random().Next(1, 100);
+Console.WriteLine("Второе рандомное число равно: " + number2);
 int sum = number1 + number2;
 Console.WriteLine("Сумма введенных чисел будет равна: " + sum);
